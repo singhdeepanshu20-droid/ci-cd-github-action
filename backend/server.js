289d9e4dd2546beb628ffa0f0c119c12d75991a7
Deepanshu Singh <singhdeepanshu20@gmail.com>
2026-09-24 16:05:55 +1000

    Making some changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.get("/api/message", (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
